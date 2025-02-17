8060da8084bd397489fbd32f817383ce1409f15a
R4fael94 <rafaelcinfo330@gmail.com>
2025-02-17 19:53:42 -0300

    Update questao4.py

--- questao4.py
+++ questao4.py
@@ -1,7 +1,7 @@
-not1 = int(input("Informe a 1º Nota: "))
-not2 = int(input("Informe a 2º Nota: "))
-not3 = int(input("Informe a 3º Nota: "))
-not4 = int(input("Informe a 4º Nota: "))
+not1 = float(input("Informe a 1º Nota: "))
+not2 = float(input("Informe a 2º Nota: "))
+not3 = float(input("Informe a 3º Nota: "))
+not4 = float(input("Informe a 4º Nota: "))
 média = (not1+not2+not3+not4)/4
 
 print(f"Sua média é: {média}")
