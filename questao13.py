e35024cb07a217cb913c246cf166c4432eca0db3
R4fael94 <rafaelcinfo330@gmail.com>
2025-02-24 20:07:59 -0300

    Update questao13.py

---
+++ questao13.py
@@ -0,0 +1,5 @@
+alt = float(input("Qual é sua altura? "))
+pesoIdealH = (72.7*alt)-58
+pesoIdealM = (62.1*alt)-44.7
+
+print(f"O peso ideal para homem é: {pesoIdealH}kg e para mulher {pesoIdealM}kg")
