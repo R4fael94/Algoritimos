91f5b742b22d48ca77ba614f05f32bea28e17f26
R4fael94 <rafaelcinfo330@gmail.com>
2025-02-24 21:28:13 -0300

    update
    questão 4 e nova questão 13

--- questao13.py
+++ questao13.py
@@ -1,5 +1,9 @@
 alt = float(input("Qual é sua altura? "))
+sexo = input("Você é homem ou mulher? ")
 pesoIdealH = (72.7*alt)-58
 pesoIdealM = (62.1*alt)-44.7
 
-print(f"O peso ideal para homem é: {pesoIdealH}kg e para mulher {pesoIdealM}kg")
+if (sexo == "homem"):
+    print(f"O peso ideal para homem é: {pesoIdealH}kg")
+else :
+    print(f"O peso ideal para mulher é: {pesoIdealM}kg")
--- questao4.py
+++ questao4.py
@@ -2,6 +2,8 @@ not1 = float(input("Informe a 1º Nota: "))
 not2 = float(input("Informe a 2º Nota: "))
 not3 = float(input("Informe a 3º Nota: "))
 not4 = float(input("Informe a 4º Nota: "))
-média = (not1+not2+not3+not4)/4
+media = (not1+not2+not3+not4)/4
 
-print(f"Sua média é: {média}")
+print(f"Sua média é: {media}")
+if media >=7:
+	print("Você foi aprovado!")
